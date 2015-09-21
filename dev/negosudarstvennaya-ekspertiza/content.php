ec5df8f843e91f6874f32634361bc03d9e2d0074
ilcomp <cgde88@gmail.com>
2015-09-21 13:13:42 +0300

    u

---
+++ dev/negosudarstvennaya-ekspertiza/content.php
@@ -0,0 +1,155 @@
+<?php $root='../';
+include($root."blocks/meta.php");
+include($root."blocks/header.php");?> 
+
+<article>
+   <div class="container">
+         <h1>Негосударственная экспертиза проектной документации</h1>
+		<p>
+Лучшее предложение по проведению негосударственной экспертизы проектной документации
+		</p><p>
+Предлагаем одни из самых выгодных в стране условий по срокам, оплате, гарантиям.
+		</p><p>
+<b>Экспертиза проектно-сметной документации</b>
+		</p><p>
+В короткие сроки и за минимальную стоимость проводим негосударственную экспертизу проектно-сметной документации. Наши цены существенно дешевле, чем официальные цены госэкспертизы. Одни из немногих среди экспертных компаний, мы не требуем предоплату.<br>
+Работаем по всей стране, принимаем документы на экспертизу в электронном виде. Обеспечиваем качественное и ответственное рассмотрение проектно-сметной документации силами более чем 50 опытных аттестованных экспертов. Предлагаем одни из самых выгодных в стране условий по срокам, оплате, гарантиям.<br>
+Гарантируем минимальный срок экспертизы. В случае Вашей особой потребности - всегда готовы к обсуждению максимально коротких сроков.<br>
+Оказываем реальную помощь и поддержку: в случае обнаружения недостатков в проектно-сметной документации наши эксперты немедленно поставят Вас в известность об этом и дадут подробные консультации по необходимым исправлениям.
+		</p><p>
+<b>Экспертиза результатов инженерных изысканий</b>
+		</p><p>
+На отличных условиях, в короткие сроки и за минимальную стоимость проводим негосударственную экспертизу результатов инженерных изысканий. Более 20 наших опытных экспертов готовы оказать Вам эту услугу.
+		</p><p>
+<b>Обращайтесь к нам!</b>
+		</p><p>
+Возможно, мы предлагаем лучшие условия.<br>
+Если Вы где-то найдете условия еще лучше - сообщите нам, и мы предложим Вам еще лучшие условия плюс дополнительную скидку 5%.<br>
+Мы не можем гарантировать положительное заключение экспертизы в 100% случаев, поскольку мы проводим реальную экспертизу, а не просто выдаем заключения. Но мы обещаем что Вы, при необходимости, получите от нас всю необходимую помощь и поддержку, направленную на то, чтобы Ваш проект полностью соответствовал требованиям.<br>
+Мы постараемся сделать все возможное, чтобы сотрудничество с нами Вампонравилось и Вы стали нашим постоянным заказчиком.
+		</p><p>
+<b>Выбирайте между госэкспертизой и негосударственной экспертизой</b>
+		</p><p>
+В тех случаях, когда закон допускает возможность обращения в негосударственную экспертизу, Вам достаточно просто сравнить условия, чтобы сделать выбор:
+		<ul>
+<li>цены дешевле (минус 30% от цены госэкспертизы)</li>
+<li>нет предоплаты</li>
+<li>сроки быстрее ( в 2-3 раза быстрее сроков госэкспертизы)</li>
+<li>помогаем ( нет цели вернуть проект на доработку и получить оплату за повторную экспертизу)</li>
+<li>удобнее (отправляете проект электронно, никуда не надо ездить)</li>
+		</ul></p><p>
+<b>Есть сомнения? Позвоните нам  и задайте любые свои вопросы.</b>
+	</p>
+		<div class="row">
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image04.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Быстро</b><br>
+Срок проведения экспертизы 15 рабочих дней.<br>
+Всегда в 2-3 раза быстрее официально установленных сроков госэкспертизы! Узнайте о наших гарантиях сроков - они Вас обрадуют.</p></div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image07.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Просто</b><br>
+Вам никуда не надо выходить из своего офиса.<br>
+Отправляете документы электронкой. Получаете Заключение экспертизы с ЭЦП. Желаете подлинник заключения? Пришлем DHL в срок 2 дня.</p></div>
+				</div>
+			</div>
+<div class="clearfix visible-sm visible-md"></div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image06.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Честно</b><br>
+Также как и госэкспертиза, мы считаем цену согласно  Постановлению Правительства №145, а не выдумываем ее исходя из платежеспособности заказчика.</p></div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image10.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Выгодно</b><br>
+Обратитесь к нам во второй раз - получите скидку 5%. В третий - 10%. И так далее. Вам это точно понравится.
+</p></div>
+				</div>
+			</div>
+<div class="clearfix visible-sm visible-md visible-lg"></div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image08.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Дешево</b><br>
+После того как мы посчитали цену по Постановлению, мы вычитаем из нее 30% Да, именно так! Наша цена всегда на 30% ниже, чем официально установленные цены госэкспертизы!
+</p></div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image12.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Помогаем</b><br>
+Мы гарантируем,  что с нами Вы никогда не столкнетесь с бесконечными возвратами проектной документации на доработку. Если мы находим ошибку - мы немедленно поможем Вам ее исправить.
+</p></div>
+				</div>
+			</div>
+<div class="clearfix visible-sm visible-md"></div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image11.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Удобно</b><br>
+У Вас будет персональный менеджер. Вы можете звонить ему в любое время.<br>
+Он даст ответ на любой вопрос и всегда поможет.
+</p></div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="row">
+					<div class="col-xs-4 col-sm-4"><img src="img/image13.png" alt="" class="img-max center-block"></div>
+					<div class="col-xs-8 col-sm-8"><p><b>Доверяем Вам</b><br>
+Никаких предоплат!<br>
+Мы получаем оплату только после того как сделали свою работу. А не наоборот.<br>
+Доверяете нам? Хотите заплатить предоплату?<br>
+Тогда наш ответный подарок - дополнительная скидка 10%.</p></div>
+				</div>
+			</div>
+		</div>
+		
+<p class="text-center"><b>5 шагов к положительному заключению экспертизы:</b></p>
+		<div class="row">
+			<div class="col-sm-4 col-lg-2 col-lg-offset-1">
+				<div class="row">
+					<div class="col-xs-9 col-sm-9 text-center"><div class="border-circle-yellow">1</div><p><b>Обратитесь к нам</b></p>
+						<p>Позвоните и mотправьте заявку</p></div>
+					<div class="col-xs-3 col-sm-3"><img src="img/image01.png" alt="" class="img-responsive"></div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-lg-2">
+				<div class="row">
+					<div class="col-xs-9 col-sm-9 text-center"><div class="border-circle-yellow">2</div><p><b>Расчёт цены</b></p>
+						<p class="color-red">максимум 2 часа</p><p>всегда на 30% дешевле государственной экспертизы</p></div>
+					<div class="col-xs-3 col-sm-3"><img src="img/image01.png" alt="" class="img-responsive"></div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-lg-2">
+				<div class="row">
+					<div class="col-xs-9 col-sm-9 text-center"><div class="border-circle-yellow">3</div><p><b>Подписание договора.</b></p>
+						<p><b>Направление Документов</b></p><p class="color-red">1 день</p></div>
+					<div class="col-xs-3 col-sm-3"><img src="img/image01.png" alt="" class="img-responsive"></div>
+				</div>
+			</div>
+<div class="clearfix visible-sm visible-md"></div>
+			<div class="col-sm-4 col-lg-2 col-sm-offset-2 col-lg-offset-0">
+				<div class="row">
+					<div class="col-xs-9 col-sm-9 text-center"><div class="border-circle-yellow">4</div><p><b>Экспертиза</b></p>
+						<p class="color-red">14 дней (срочно - 5 дней)</p></div>
+					<div class="col-xs-3 col-sm-3"><img src="img/image01.png" alt="" class="img-responsive"></div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-lg-2">
+				<div class="row">
+					<div class="col-xs-9 col-sm-9 text-center"><div class="border-circle-yellow">5</div><p><b>Заключение экспертизы.</b></p>
+						<p><b>Подписание акта. Оплата по факту.</b></p></div>
+				</div>
+			</div>
+		</div>
+   </div><!-- container -->
+</article>
+<?php include($root."blocks/footer.php"); ?>
--- dev/sertifikaty/index.php
+++ dev/sertifikaty/index.php
@@ -1,18 +1,7 @@
-<?php $title = 'Сертификаты'; ?>
-<?php $description = ''; ?>
-<?php $keywords = ''; ?>
-<?php $root='../';
-include($root."blocks/meta.php");
-include($root."blocks/header.php");?> 
+<?php
+$title = 'Сертификаты';
+$description = '';
+$keywords = '';
 
-<article>
-   <div class="container">
-         <h1>Сертификаты</h1>
-<?php 
-$photo_col='col-md-3';
-$photo_alt='Сертификаты строительства, экспертизы и оценки';
-include($root."blocks/img.php");
+include("content.php");
 ?>
-   </div><!-- container -->
-</article>
-<?php include($root."blocks/footer.php"); ?>
