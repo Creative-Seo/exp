37a31cf6c3b9c9f898c25162fe0ac5643d1cf44c
ilcomp <cgde88@gmail.com>
2015-09-21 10:43:14 +0300

    u

--- dev/blocks/navbar.php
+++ dev/blocks/navbar.php
@@ -14,39 +14,39 @@
       <li class="dropdown">
         <a href="http://fezvrasta.github.io/bootstrap-material-design/bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">Экспертиза <i class="mdi-navigation-expand-more" style="font: 20px/1 'Material-Design-Icons';vertical-align:bottom;"></i></a>
         <ul class="dropdown-menu">
-          <li><a href="/dosudebnaya-stroitelnaya-ekspertiza">Досудебная строительная</a></li>
-          <li><a href="/ekspertiza-ushcherba-posle-zaliva-kvartiry">Ущерб после залива квартиры</a></li>
-          <li><a href="/ekspertiza-ushcherba-pri-stihiynyh-bedstviyah">Ущерб при стихийных бедствиях</a></li>
-          <li><a href="/negosudarstvennaya-ekspertiza">Негосударственная проектной документации</a></li>
-          <li><a href="/ocenochnaya-ekspertiza">Оценочная</a></li>
-          <li><a href="/avtotehnicheskaya">Автотехническая</a></li>
-          <li><a href="/tovarovedcheskaya-ekspertiza">Товароведческая</a></li>
-          <li><a href="/ekspertiza-okruzhayushchey-sredy">Окружающая среда</a></li>
-          <li><a href="/metallovedcheskaya-ekspertiza">Металловедческая</a></li>
-          <li><a href="/zemleustroitelnaya-ekspertiza">Землеустроительная</a></li>
-          <li><a href="/ekspertiza-zdaniy">Здания и сооружения</a></li>
-          <li><a href="/stroitelno-tehnicheskaya-ekspertiza">Строительно-техническая</a></li>
-          <li><a href="/ekspertiza-inzhenernyh-kommunikaciy">Инженерных коммуникаций</a></li>
+          <li><a href="/dosudebnaya-stroitelnaya-ekspertiza/">Досудебная строительная</a></li>
+          <li><a href="/ekspertiza-ushcherba-posle-zaliva-kvartiry/">Ущерб после залива квартиры</a></li>
+          <li><a href="/ekspertiza-ushcherba-pri-stihiynyh-bedstviyah/">Ущерб при стихийных бедствиях</a></li>
+          <li><a href="/negosudarstvennaya-ekspertiza/">Негосударственная проектной документации</a></li>
+          <li><a href="/ocenochnaya-ekspertiza/">Оценочная</a></li>
+          <li><a href="/avtotehnicheskaya/">Автотехническая</a></li>
+          <li><a href="/tovarovedcheskaya-ekspertiza/">Товароведческая</a></li>
+          <li><a href="/ekspertiza-okruzhayushchey-sredy/">Окружающая среда</a></li>
+          <li><a href="/metallovedcheskaya-ekspertiza/">Металловедческая</a></li>
+          <li><a href="/zemleustroitelnaya-ekspertiza/">Землеустроительная</a></li>
+          <li><a href="/ekspertiza-zdaniy/">Здания и сооружения</a></li>
+          <li><a href="/stroitelno-tehnicheskaya-ekspertiza/">Строительно-техническая</a></li>
+          <li><a href="/ekspertiza-inzhenernyh-kommunikaciy/">Инженерных коммуникаций</a></li>
         </ul>
       </li>
       <li class="dropdown">
         <a href="http://fezvrasta.github.io/bootstrap-material-design/bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">Оценка <i class="mdi-navigation-expand-more" style="font: 20px/1 'Material-Design-Icons';vertical-align:bottom;"></i></a>
         <ul class="dropdown-menu">
-          <li><a href="/ocenka-nedvizhimosti">Недвижимость</a></li>
-		  <li><a href="/ocenka-kvartiry">Квартира</a></li>
-		  <li><a href="/ocenka-avtomobilya">Автомобиль</a></li>
-		  <li><a href="/ocenka-zemli">Земля</a></li>
-		  <li><a href="/ocenka-biznesa">Бизнес</a></li>
-		  <li><a href="/ocenka-mashin-i-oborudovaniya">Машины и оборудование</a></li>
-		  <li><a href="/ocenka-ushcherba-pri-pozhare">Ущерб при пожаре</a></li>
+          <li><a href="/ocenka-nedvizhimosti/">Недвижимость</a></li>
+		  <li><a href="/ocenka-kvartiry/">Квартира</a></li>
+		  <li><a href="/ocenka-avtomobilya/">Автомобиль</a></li>
+		  <li><a href="/ocenka-zemli/">Земля</a></li>
+		  <li><a href="/ocenka-biznesa/">Бизнес</a></li>
+		  <li><a href="/ocenka-mashin-i-oborudovaniya/">Машины и оборудование</a></li>
+		  <li><a href="/ocenka-ushcherba-pri-pozhare/">Ущерб при пожаре</a></li>
         </ul>
       </li>
       <li class="dropdown">
         <a href="http://fezvrasta.github.io/bootstrap-material-design/bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">Строительство <i class="mdi-navigation-expand-more" style="font: 20px/1 'Material-Design-Icons';vertical-align:bottom;"></i></a>
         <ul class="dropdown-menu">
-          <li><a href="/dekorativnaya-otdelka">Декоративная отделка</a></li>
-          <li><a href="/dizayn-interera">Дизайн интерьера</a></li>
-          <li><a href="/otdelka-fasada-doma">Отделка фасада дома</a></li>
+          <li><a href="/dekorativnaya-otdelka/">Декоративная отделка</a></li>
+          <li><a href="/dizayn-interera/">Дизайн интерьера</a></li>
+          <li><a href="/otdelka-fasada-doma/">Отделка фасада дома</a></li>
         </ul>
       </li>
       <li><a href="javascript:void(0);">Цены</a></li>
@@ -54,8 +54,8 @@
         <a href="http://fezvrasta.github.io/bootstrap-material-design/bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">О нас <i class="mdi-navigation-expand-more" style="font: 20px/1 'Material-Design-Icons';vertical-align:bottom;"></i></a>
         <ul class="dropdown-menu">
 		  <li><a href="javascript:void(0);">О компании</a></li>
-		  <li><a href="/sertifikaty">Сертификаты</a></li>
-		  <li><a href="/contacts">Контакты</a></li>
+		  <li><a href="/sertifikaty/">Сертификаты</a></li>
+		  <li><a href="/contacts/">Контакты</a></li>
         </ul>
       </li>
     </ul>
